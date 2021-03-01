6d83cdac657c747e65b1b4ce4557830edc787b54
Ahmad <benos.tarabain@gmail.com>
2021-03-01 02:25:25 +0200

    final product

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,17 @@ function App() {
       <Router>
         <div className="container">
           <h1 className="text-center mt-2">Crypto Prices</h1>
+          <p className="text-center">
+            React, GraphQL, Apollo, and coinlore(API) project
+          </p>
+          <p className="text-center">
+            <a href="https://github.com/ahmadbenos/graphql-apollo_cryptocurrency">
+              By Ahmad Tarabein
+              <span>
+                <i class="fab fa-github"></i>
+              </span>
+            </a>
+          </p>
           <Route path="/" exact component={Coins} />
           <Route path="/details/:id" component={CoinDetails} />
         </div>
